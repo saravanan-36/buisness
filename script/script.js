b96b48b21878126030c2485c6cccb8ca2c3872a7
saravanan-36 <saravananbs0369@gmail.com>
2025-03-02 20:13:40 +0530

    updated

---
+++ script/script.js
@@ -0,0 +1,8 @@
+var sidenav=document.querySelector(".side-navbar");
+sidenav.style.display="none"
+function showNavBar() {
+    sidenav.style.display="block"
+}
+function closeNavBar() {
+     sidenav.style.display="none"
+}
